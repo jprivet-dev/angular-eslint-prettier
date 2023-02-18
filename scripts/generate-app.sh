b19b7ef8e8dc378483f3772984e415933b16caed
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-18 14:22:00 +0100

    feat(generate-app): update echos & commands

--- scripts/generate-app.sh
+++ scripts/generate-app.sh
@@ -3,6 +3,10 @@
 # Usage:
 # $ . scripts/generate-app.sh
 
+echo "######################################################"
+echo "# Generate a new Angular app using ESlint & Prettier #"
+echo "######################################################"
+
 echo "> Remove the old Angular app"
 rm -rf app
 
@@ -15,3 +19,13 @@ ng add @angular-eslint/schematics
 
 echo "> Generate a new Angular app in the workspace using ESLint"
 ng generate @angular-eslint/schematics:application app --project-root --defaults --routing --style scss --strict
+
+echo "> Install Prettier locally"
+npm install --save-dev --save-exact prettier
+
+echo "> Create an empty config file"
+echo {}> .prettierrc.json
+cp .gitignore .prettierignore
+
+echo "> Format all files with Prettier"
+npx prettier --write .

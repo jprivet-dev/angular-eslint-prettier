010f5a2a0a5ef1687d56b5f2dcd3b8a6c9a41b14
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-18 13:53:52 +0100

    feat(scripts): file renaming

--- scripts/generate-app.sh
+++ scripts/generate-app.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 # Usage:
-# $ . scripts/case-1.sh
+# $ . scripts/generate-app.sh
 
-#ng new app --skip-git true --style scss --defaults --strict
-ng new app --create-application false
+ng new app --create-application false --force
 cd app
 ng add @angular-eslint/schematics
 ng generate @angular-eslint/schematics:application app --project-root --routing --style scss --strict

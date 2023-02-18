b411cb2dc6558a6576c08f7ee7f04a007a01fed6
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-18 14:14:17 +0100

    feat(scripts): adjust options

--- scripts/generate-app.sh
+++ scripts/generate-app.sh
@@ -7,11 +7,11 @@ echo "> Remove the old Angular app"
 rm -rf app
 
 echo "> Generate an empty Angular workspace"
-ng new app --create-application false --force
+ng new app --create-application false --defaults
 cd app
 
 echo "> Install ESLint"
 ng add @angular-eslint/schematics
 
 echo "> Generate a new Angular app in the workspace using ESLint"
-ng generate @angular-eslint/schematics:application app --project-root --routing --style scss --strict
+ng generate @angular-eslint/schematics:application app --project-root --defaults --routing --style scss --strict

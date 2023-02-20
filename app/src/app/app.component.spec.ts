b70091dbb059eb547a4b1d13da449ba3d010df25
jprivet-dev <jean.philippe.rivet@gmail.com>
2023-02-20 08:00:24 +0100

    build(ng): generate new app

--- app/src/app/app.component.spec.ts
+++ app/src/app/app.component.spec.ts
@@ -5,8 +5,12 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -26,8 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'app app is running!'
-    );
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app app is running!');
   });
 });
